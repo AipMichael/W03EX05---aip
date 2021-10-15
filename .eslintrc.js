f17475379422888117e8e80a8526557589b2241b
AipBorges <mpbds@uic.es>
2021-10-15 09:56:29 +0200

    first files added

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "lines-between-class-members": "off",
+    "class-methods-use this": "off",
+    "import/extensions": "off",
+  },
+};
